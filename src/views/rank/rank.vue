d15db253afa775f96db92e3cfa1d0a9652f8fd4f
tannnb <tanbingwork@163.com>
2018-08-14 00:08:25 +0800

    侧边样式编写

--- src/views/rank/rank.vue
+++ src/views/rank/rank.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="rank">
-    <div>暂未开发...o(╥﹏╥)o</div>
+  <div class="rankWrapper">
+    <div class="rank-main">
+      <div class="slider">
+        <ul @click.stop class="slider-items" clas v-for="(items,index) in listOpt" :key="items.GroupID">
+          <span class="title">{{items.GroupName}}</span>
+          <li class="item"
+              v-for="(item,index) in items.List"
+              :class="currentIndex === item.topID? 'active':''  "
+              @click.stop="handleSelectSlider(item.topID)"
+              :key="item.topID"> {{item.ListName}}</li>
+        </ul>
+      </div>
+      <div class="rankContent">
+        sdf
+      </div>
+    </div>
+    <div></div>
   </div>
 </template>
 
@@ -12,7 +27,8 @@
     name: "rank",
     data() {
       return {
-        listOpt: []
+        listOpt: [],
+        currentIndex:0
       }
     },
     created() {
@@ -31,6 +47,10 @@
           .catch(err => {
             console.log(err)
           })
+      },
+      handleSelectSlider(id){
+        console.log(id)
+       this.currentIndex = id
       }
     }
   }
@@ -38,8 +58,49 @@
 
 <style lang="stylus" scoped>
 
-  .rank {
-    text-align center
+  .rankWrapper {
+    padding-top 35px
+    background: linear-gradient(#f3f3f3, #fff);
+    .rank-main {
+      display flex
+      width 1200px
+      margin 0 auto
+      .slider {
+        flex 0 0 150
+        width 150px
+        border:1px solid rgba(153,153,153,.2)
+        .slider-items{
+          .title{
+            display block
+            line-height: 60px;
+            font-size: 20px;
+            font-weight: 400;
+            font-size 15px
+            text-align center
+            border-bottom: 1px solid #ebebeb;
+            margin: 0 17px 10px;
+          }
+          .item{
+            display: block;
+            text-align center
+            line-height: 22px;
+            padding: 8px 17px;
+            font-size 14px
+            cursor pointer
+            &:hover{
+              color: #31c27c
+            }
+            &.active{
+              color: #fff
+              background: #31c27c
+            }
+          }
+        }
+      }
+      .rankContent {
+        flex 1
+      }
+    }
   }
 
 </style>
